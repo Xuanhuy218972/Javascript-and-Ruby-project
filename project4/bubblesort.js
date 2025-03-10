6cbe26fd419817a2f7d6d2a4675d9cbfdce3d92f
Xuanhuy218972 <xuanhuy21082023@gmail.com>
2025-03-10 20:18:49 +0700

    Use 2 for loop

--- project4/bubblesort.js
+++ project4/bubblesort.js
@@ -30,16 +30,25 @@ function parseNumbersFromFile(filePath) {
 
 function bubbleSort(arr) {
     let n = arr.length;
-    do {
         for (let i = 0; i < n - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+            for (let j = 0; j  < n - 1 - i; j++) {
+                if (arr[j]  > arr[j + 1]) {
+                    // [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                    let x = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = x;
+                }
             }
         }
-        n--;
-    } while (n != 0);
-    return arr;
-}
+        return arr;
+    }
+            //         if (arr[i] > arr[i + 1]) {
+            //             [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+            //         }
+            //     }
+            //     n--;
+            // } while (n != 0);
+            // return arr;
 
 console.log(sortedAscending.join(", "));
 console.log(sortedDescending.join(", "));
